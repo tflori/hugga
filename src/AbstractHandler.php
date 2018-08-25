c40a1cc1f464fc9a609ea03334923005b4b0a802
Thomas Flori <thflori@gmail.com>
2018-08-25 10:56:24 +0200

    ignore coverage for copied polyfill
    It's not possible to test the method completely on a single machine.

--- src/AbstractHandler.php
+++ src/AbstractHandler.php
@@ -27,6 +27,11 @@ abstract class AbstractHandler
         $this->resource = $resource;
     }
 
+    /**
+     * @param $resource
+     * @return bool|void
+     * @codeCoverageIgnore polyfill from https://github.com/symfony/polyfill/blob/master/src/Php72/Php72.php
+     */
     protected static function isTty($resource)
     {
         if (function_exists('stream_isatty')) {
